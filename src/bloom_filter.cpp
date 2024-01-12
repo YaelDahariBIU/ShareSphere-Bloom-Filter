7d5b0902c1e97844b9ffd741ba29a3f20aca6367
Tal144155 <tal.ariel.ziv@gmail.com>
2024-01-12 13:43:55 +0200

    adding first sanity checks

---
+++ src/bloom_filter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tal on 12/01/2024.
+//
+
+#include "bloom_filter.h"
+int main1() {
+    int x=5;
+    return 0;
+}
---
+++ tests/bf_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tal on 12/01/2024.
+//
+#include <gtest/gtest.h>
+#include "bloom_filter.h"
+
+TEST(BloomFilter, BasicTest) {
+    ASSERT_EQ(main1(), 0);
+}
+
