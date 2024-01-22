abf25fe45a5c88c6af95fde54f728d0ee7ae3041
Tal144155 <tal.ariel.ziv@gmail.com>
2024-01-22 23:02:17 +0200

    adding new HashTest for the new implementation.

--- tests/BFArrayTest.cpp
+++ tests/BFArrayTest.cpp
@@ -6,6 +6,8 @@
 #include "BloomFilter/BloomFilterArray.h"
 #include "BloomFilter/BlackList.h"
 #include "BloomFilter/Filter.h"
+#include "Hash/HashOne.h"
+#include "Hash/HashTwo.h"
 
 TEST(BloomFilterArrayTest, BasicTests) {
     //adding new bloom filter array
@@ -34,17 +36,24 @@ TEST(BlackListURLTest, BasicTests) {
     ASSERT_EQ(blackList.doesExist("www.com2"), true);
 }
 
-TEST(FilterTest, BasicTests) {
-    //creating filter object using static method.
-    Filter* filter = Filter::getInstance();
-    //adding first url
-    blackList.add("www.com");
-    //checking if it was added and if another one is not
-    ASSERT_EQ(blackList.doesExist("www.com"), true);
-    ASSERT_EQ(blackList.doesExist("www.com2"), false);
-    //adding it and checking again.
-    blackList.add("www.com2");
-    ASSERT_EQ(blackList.doesExist("www.com2"), true);
+TEST(HashOneTwoTest, CheckingHashOneTwo) {
+    //creating hash objects.
+    HashOne hashOne = HashOne();
+    HashTwo hashTwo = HashTwo();
+    //checking for the output as wanted
+    ASSERT_EQ(hashOne.hashing("www.example.com0"), 8231510004620773819);
+    ASSERT_EQ(hashTwo.hashing("www.example.com0"),  6657855685155196946);
+}
+
+TEST(HashSetTest, CheckingHashTest) {
+    //creating hash set objects.
+    HashSet hashSet = HashSet();
+    //checking for the size of has set.
+    ASSERT_EQ(hashSet.getSize(),2);
+    //checking if the hash result match the excepted output.
+    ASSERT_EQ(hashSet.getHashed(1,"www.example.com0"), 8231510004620773819);
+    ASSERT_EQ(hashSet.getHashed(2,"www.example.com0"), 6657855685155196946);
+
 }
 
 
